067d4512dfd6fa3f697c0b7ea44377c1b9eb13c4
nfnat0 <mjtmwm@gmail.com>
2024-05-31 09:52:14 +0900

    chore: Update .env files and API URL in boardService

--- src/api/boardService.js
+++ src/api/boardService.js
@@ -1,31 +1,52 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_URL = 'http://localhost:5000/boards';
+const API_URL = process.env.REACT_APP_API_URL;
 
 const getBoards = async () => {
-  const response = await axios.get(API_URL);
-  return response.data;
+  try {
+    const response = await axios.get(API_URL);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching boards:", error);
+    throw error;
+  }
 };
 
 const addBoard = async (board) => {
-  const response = await axios.post(API_URL, board);
-  return response.data;
+  try {
+    const response = await axios.post(API_URL, board);
+    return response.data;
+  } catch (error) {
+    console.error("Error adding board:", error);
+    throw error;
+  }
 };
 
 const editBoard = async (board) => {
-  const response = await axios.put(`${API_URL}/${board.id}`, board);
-  return response.data;
+  try {
+    const response = await axios.put(`${API_URL}/${board.id}`, board);
+    return response.data;
+  } catch (error) {
+    console.error("Error editing board:", error);
+    throw error;
+  }
 };
 
 const deleteBoard = async (boardId) => {
-  const response = await axios.delete(`${API_URL}/${boardId}`);
-  return response.data;
+  try {
+    const response = await axios.delete(`${API_URL}/${boardId}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error deleting board:", error);
+    throw error;
+  }
 };
+
 const boardService = {
-    getBoards,
-    addBoard,
-    editBoard,
-    deleteBoard,
+  getBoards,
+  addBoard,
+  editBoard,
+  deleteBoard,
 };
 
 export default boardService;
