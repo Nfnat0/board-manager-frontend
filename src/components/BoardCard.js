744bdc0fceb915da582face08b647cf1eb57239a
nfnat0 <mjtmwm@gmail.com>
2024-05-31 22:29:33 +0900

    chore: Add CSS for BoardCard component

--- src/components/BoardCard.js
+++ src/components/BoardCard.js
@@ -3,10 +3,11 @@ import React from 'react';
 import { Card, CardContent, Typography, IconButton } from '@material-ui/core';
 import EditIcon from '@material-ui/icons/Edit';
 import DeleteIcon from '@material-ui/icons/Delete';
+import './BoardCard.css'; // Import the CSS file for custom styles
 
 const BoardCard = ({ board, onEditClick, onDelete }) => {
   return (
-    <Card>
+    <Card className="boardCard">
       <CardContent>
         <Typography variant="h5">{board.icon} {board.title}</Typography>
         <Typography variant="body2">{board.description}</Typography>
