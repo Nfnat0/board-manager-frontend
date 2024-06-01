90612a1a61355ea5914b6754a92e251d70a4bdb0
nfnat0 <mjtmwm@gmail.com>
2024-06-01 09:24:19 +0900

    chore: Update Header component to include link to home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,14 @@
 // src/components/Header.js
 import React from 'react';
 import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <AppBar position="static" style={{ backgroundColor: 'rgba(63, 81, 181, 0.6)' }}>
       <Toolbar>
         <Typography variant="h6">
-          Board Manager
+          <Link to="/" style={{ color: 'white', textDecoration: 'none' }}>Board Manager</Link>
         </Typography>
       </Toolbar>
     </AppBar>
